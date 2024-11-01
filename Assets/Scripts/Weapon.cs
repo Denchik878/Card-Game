b0ced2d5a0280d8e108792bd984d057ba16027b8
Denchik878 <148837802+Denchik878@users.noreply.github.com>
2024-10-31 21:03:25 -0700

    Обнова

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Card
+{
+    public GameObject[] elements;
+    public int turnsToAttack;
+    public override void Turn()
+    {
+        if (turnsToAttack <= 1)
+        {
+            Destroy(gameObject);//Будет баг
+        }
+        else
+        {
+            turnsToAttack--;
+            StartCoroutine(FadeAndDestroy(elements[turnsToAttack]));
+        }
+    }
+    private IEnumerator FadeAndDestroy(GameObject element)
+    {
+        SpriteRenderer renderer = element.GetComponent<SpriteRenderer>();
+        Color color = renderer.color;
+        float alpha = color.a;
+        while (alpha > 0)
+        {
+            yield return null;
+            alpha -= Time.deltaTime;
+            color.a = alpha;
+            renderer.color = color;
+        }
+        Destroy(element);
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
