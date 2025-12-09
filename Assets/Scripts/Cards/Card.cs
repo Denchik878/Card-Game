dbe8b647eda5b0eb17a8f8b31bff427375d3220f
Denchik878 <148837802+Denchik878@users.noreply.github.com>
2025-12-08 21:18:27 -0800

    Card effect script i over mnogo chego eshe

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -13,6 +13,7 @@ public abstract class Card : MonoBehaviour
     public TMP_Text damageText;
     public bool isEnemy;
     private CardEffect[] effects;
+    private List<GameObject> iconEffects = new();
     public event Action<Card> OnDeath;
     public float animationSpeed = 1;
     public float crystalAnimDura = 0.25f;
@@ -23,18 +24,25 @@ public abstract class Card : MonoBehaviour
     public AudioClip healingSound;
     public int valutaErned;
     public int crystalsAddedByAbility;
+    private Collider2D collider;
+    private Vector3[] iconPositions;
+
     protected void Start()
     {
+        iconPositions = IconPositions();
         ChangeCrystalAmount(0);
         if (damageText != null)
         {
             damageText.text = damage.ToString();
         }
+
         if (HPText != null)
         {
             ChangeHealth(maxHealth);
-        }        
+        }
+        collider = GetComponent<Collider2D>();
     }
+
     public void ChangeHealth(int a)
     {
         currentHealth += a;
@@ -42,6 +50,7 @@ public abstract class Card : MonoBehaviour
         {
             currentHealth = maxHealth;
         }
+
         if (currentHealth <= 0)
         {
             currentHealth = 0;
@@ -56,8 +65,10 @@ public abstract class Card : MonoBehaviour
         {
             AudioManager.Instance.PlayClip(healingSound);
         }
+
         HPText.text = currentHealth.ToString();
     }
+
     public async Awaitable BaseTurn()
     {
         effects = GetComponents<CardEffect>();
@@ -68,54 +79,80 @@ public abstract class Card : MonoBehaviour
                 effect.TakeEffect();
             }
         }
+
         await Turn();
     }
+
     protected virtual async Awaitable Turn()
     {
-        
+
     }
+
     public async Awaitable ChangeCrystalAmount(int amount)
     {
         crystalAmount += amount;
         Vector3[] positions = CrystalsPositions(crystalAmount);
-        foreach(GameObject crystal in crystals)
+        float timer = 0;
+        while (timer < crystalAnimDura)
         {
-            float timer = 0;
-            var render = crystal.GetComponent<SpriteRenderer>();
-            while (timer < crystalAnimDura)
+            await Awaitable.NextFrameAsync();
+            timer += Time.deltaTime;
+            foreach (GameObject crystal in crystals)
             {
-                await Awaitable.NextFrameAsync();
-                timer += Time.deltaTime;
+                var render = crystal.GetComponent<SpriteRenderer>();
                 render.material.SetFloat("_Dissolved", 1 - timer / crystalAnimDura);
             }
+        }
+
+        foreach (var crystal in crystals)
+        {
             Destroy(crystal);
         }
+
         crystals.Clear();
-        foreach(Vector3 position in positions)
+        foreach (Vector3 position in positions)
         {
             GameObject newCrystal = Instantiate(crystal, transform.TransformPoint(position),
                 Quaternion.identity, transform);
             crystals.Add(newCrystal);
+            var render = newCrystal.GetComponent<SpriteRenderer>();
+            render.material.SetFloat("_Dissolved", 0);
+        }
+
+        timer = 0;
+        while (timer < crystalAnimDura)
+        {
+            await Awaitable.NextFrameAsync();
+            timer += Time.deltaTime;
+            foreach (GameObject crystal in crystals)
+            {
+                var render = crystal.GetComponent<SpriteRenderer>();
+                render.material.SetFloat("_Dissolved", timer / crystalAnimDura);
+            }
         }
     }
+
     protected virtual async Awaitable DestroySelf()
     {
         OnDeath?.Invoke(this);
     }
+
     protected async Awaitable FadeAndDestroy(GameObject element)
     {
         SpriteRenderer renderer = element.GetComponent<SpriteRenderer>();
         Color color = renderer.color;
         float alpha = color.a;
-        while(alpha > 0)
+        while (alpha > 0)
         {
             await Awaitable.NextFrameAsync();
             alpha -= Time.deltaTime * animationSpeed;
             color.a = alpha;
             renderer.color = color;
         }
+
         Destroy(element);
     }
+
     private Vector3[] CrystalsPositions(int count)
     {
         Vector3[] positions = new Vector3[count];
@@ -124,17 +161,46 @@ public abstract class Card : MonoBehaviour
             positions[0] = new Vector3(0, 0.78f, 0);
             return positions;
         }
+
         if (count == 2)
         {
             positions[0] = new Vector3(-0.25f, 0.78f, 0);
             positions[1] = new Vector3(0.25f, 0.78f, 0);
             return positions;
         }
+
         float step = 1 / ((float)count - 1);
-        for(int i = 0; i < count; i++)
+        for (int i = 0; i < count; i++)
         {
             positions[i] = new Vector3(-0.5f + i * step, 0.78f, 0);
         }
+
         return positions;
+    }
+
+    public void AddEffectIcon(GameObject effect)
+    {
+        var go = Instantiate(effect);
+        iconEffects.Add(go);
+        for (int i = 0; i < iconEffects.Count; i++)
+        {
+            iconEffects[i].transform.position = iconPositions[i];
+        }
+    }
+
+    private Vector3[] IconPositions()
+    {
+        Vector3[] positions = new Vector3[20];
+        float width = collider.bounds.size.x;
+        float height = collider.bounds.size.y;
+        float gap = width / 5;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                positions[j + i*4] = new Vector3(width/2 - (j + 1)*gap, height/2 - gap, 0);
+            }
+        }
+        return null;
     }
 }
--- Assets/Scripts/GameEffects/CardEffect.cs
+++ Assets/Scripts/GameEffects/CardEffect.cs
@@ -5,6 +5,25 @@ public abstract class CardEffect : MonoBehaviour
 {
     public int turns;
     protected Card baseCard;
+    public GameObject effectIconPrefab;
+    private GameObject currentEffectIcon;
+
+    private void OnEnable()
+    {
+        if (effectIconPrefab != null)
+        {
+            currentEffectIcon = Instantiate(effectIconPrefab);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (currentEffectIcon != null)
+        {
+            Destroy(currentEffectIcon);
+        }
+    }
+
     private void Awake()
     {
         baseCard = GetComponent<Card>();
